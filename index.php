b412e6fa071ac7d45a6ac9a48942cd28befafca3
VitorEduardo7 <161628040+VitorEduardo7@users.noreply.github.com>
2025-08-22 18:22:47 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    include 'cabecalho.php'
+?>
+
+<body>
+    <div class="container">
+        <h1>Bem vindo ao meu 1° sistema web com CRUD</h1>
+        <h3>Vitor Eduardo Matos</h3>
+
+        <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+            <a href="form_cadastrar.php" class="btn btn-danger">Cadastrar</a>
+            <a href="listar.php" class="btn btn-dark">Listar</a>
+        </div>
+
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+
+</body>
+</html>
