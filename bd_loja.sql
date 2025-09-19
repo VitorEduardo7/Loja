c51c25d9fd8fc95d4aa0ed4dd5f351ad71e2e574
VitorEduardo7 <161628040+VitorEduardo7@users.noreply.github.com>
2025-09-19 18:08:15 -0300

    Add files via upload

--- bd_loja.sql
+++ bd_loja.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `loja`
 --
-CREATE DATABASE IF NOT EXISTS `loja` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `bd_loja` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `loja`;
 
 -- --------------------------------------------------------
--- form_cadastrar.php
+++ form_cadastrar.php
@@ -1,28 +1,27 @@
+<head>
+<link rel="stylesheet" href="css/style_cadastro.css">
+</head>
+
 <?php 
     include 'cabecalho.php';
 ?>
 <body>
     <div class="container">
-        <h2>CADASTRO DE PRODUTO</h2>
+        <h2>Cadastro de Produto</h2>
         <form action="inserir.php" method="POST">
             <div class="mb-3">
-                <input type="text" name="produto" class="form-control"  placeholder="Digite o nome do produto">
+                <input type="text" name="produto" class="form-control" placeholder="Digite o nome do produto" required>
             </div>
             <div class="mb-3">
-                <input type="text" name="preco" class="form-control" placeholder="Digite o preço do produto" >
+                <input type="text" name="preco" class="form-control" placeholder="Digite o preço do produto" required>
             </div>
-        <div class="mb-3">
-            <input type="text" name="quantidade" class="form-control" placeholder="Digite a Quantidade" >
-        </div>
+            <div class="mb-3">
+                <input type="text" name="quantidade" class="form-control" placeholder="Digite a quantidade" required>
+            </div>
             <button type="submit" class="btn btn-primary">Cadastrar</button>
         </form>
-    </div>
-
-
-<div class="container">
         <a href="index.php" class="btn btn-warning">Voltar</a>
-</div>
-
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 </body>
